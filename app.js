86a58c19a522552f43398a8eafb508ef0075b8be
Evan Kozliner <evankozliner@gmail.com>
2015-05-23 19:53:43 -0700

    woopsies

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ var port = process.env.PORT || 3000;
 
 app.set('port', port);
 
-console.log("Server running on port " + port);
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
