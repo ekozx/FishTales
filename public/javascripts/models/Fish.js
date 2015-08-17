45d62a98b9ed40ed9f7cc3ca6dae4f4d3ee74c59
Evan Kozliner <evankozliner@gmail.com>
2015-08-17 11:46:32 -0400

    changed from tabs to spaces --woopsies

--- public/javascripts/models/Fish.js
+++ public/javascripts/models/Fish.js
@@ -1,87 +1,87 @@
 var Fish = function() {
-	this.net = new Net([4,6,1]);
-	this.circle = new createjs.Shape();
-	this.tick = 0;
-	this.fitness = 0;
-	this.dead=0;
-	this.hunger=100;
+  this.net = new Net([4,6,1]);
+  this.circle = new createjs.Shape();
+  this.tick = 0;
+  this.fitness = 0;
+  this.dead=0;
+  this.hunger=100;
 }
 /**
  * Creates a baby fish, so cute
  * Requires male and female parents have the same number of edges in their nets.
  * @param male parent
- * 		An object of type Fish.
+ *     An object of type Fish.
  */
 Fish.prototype.makeChild = function (maleParent) {
-	var motherChromosome = this.net.getChromosome();
-	var fatherChromosome = maleParent.net.getChromosome();
+  var motherChromosome = this.net.getChromosome();
+  var fatherChromosome = maleParent.net.getChromosome();
 
-	if (fatherChromosome.length !== motherChromosome.length) {
-		console.log("Warning: Mother and father chromosomes are not same length!");
-	}
+  if (fatherChromosome.length !== motherChromosome.length) {
+    console.log("Warning: Mother and father chromosomes are not same length!");
+  }
 
-	var splitPoint = getSplitPoint(motherChromosome.length);
-	var motherSide = motherChromosome.splice(0, splitPoint);
-	var fatherSide = fatherChromosome.splice(splitPoint, fatherChromosome.length);
-	var joinedChromosome = motherSide.concat(fatherSide);
+  var splitPoint = getSplitPoint(motherChromosome.length);
+  var motherSide = motherChromosome.splice(0, splitPoint);
+  var fatherSide = fatherChromosome.splice(splitPoint, fatherChromosome.length);
+  var joinedChromosome = motherSide.concat(fatherSide);
 
-//	if(joinedChromosome.length !== fatherChromosome.length) {
-//		console.log("Joined chromosome has an incorrect size!");
-//		console.log("FATHER LEN: "+ fatherChromosome.length);
-//		console.log("MOTHER LEN: "+ motherChromosome.length);
-//		console.log("BABY   LEN: "+ joinedChromosome.length);
-//	}
+//  if(joinedChromosome.length !== fatherChromosome.length) {
+//    console.log("Joined chromosome has an incorrect size!");
+//    console.log("FATHER LEN: "+ fatherChromosome.length);
+//    console.log("MOTHER LEN: "+ motherChromosome.length);
+//    console.log("BABY   LEN: "+ joinedChromosome.length);
+//  }
 //
-	for (var i = 0; i < joinedChromosome.length; i++) {
-		if(Math.random() < getMutationRate()) {
-			joinedChromosome[i] = Math.random() * 2 - 1;
-		}
-	}
+  for (var i = 0; i < joinedChromosome.length; i++) {
+    if(Math.random() < getMutationRate()) {
+      joinedChromosome[i] = Math.random() * 2 - 1;
+    }
+  }
 
-	var fish = new Fish();
+  var fish = new Fish();
 
-	fish.circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
+  fish.circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
 
-	fish.circle.x = Math.random() * w;
-	fish.circle.y = Math.random() * h;
-
-
-	fish.circle.velY = 5;
-	fish.circle.velX = 0;
-
-	var turn = (Math.random() * 360);
-	var rad = (turn*Math.PI)/180;
-	var cs = Math.cos(rad);
-	var sn = Math.sin(rad);
-
-	var px = fish.circle.velX * cs - fish.circle.velY * sn;
-	var py = fish.circle.velX * sn + fish.circle.velY * cs;
-
-	fish.circle.velX = px;
-	fish.circle.velY = py;
-
-	fish.tick=0;
-	fish.fitness=0;
-	fish.dead=0;
-	fish.hunder=100;
+  fish.circle.x = Math.random() * w;
+  fish.circle.y = Math.random() * h;
 
 
-	fish.net.setChromosome(joinedChromosome);
-	return fish;
+  fish.circle.velY = 5;
+  fish.circle.velX = 0;
+
+  var turn = (Math.random() * 360);
+  var rad = (turn*Math.PI)/180;
+  var cs = Math.cos(rad);
+  var sn = Math.sin(rad);
+
+  var px = fish.circle.velX * cs - fish.circle.velY * sn;
+  var py = fish.circle.velX * sn + fish.circle.velY * cs;
+
+  fish.circle.velX = px;
+  fish.circle.velY = py;
+
+  fish.tick=0;
+  fish.fitness=0;
+  fish.dead=0;
+  fish.hunder=100;
+
+
+  fish.net.setChromosome(joinedChromosome);
+  return fish;
 };
 /**
  * The variance for the split.
  * @returns
- * 		An integer, x, s.t. 0<= x <= 10
+ *     An integer, x, s.t. 0<= x <= 10
  */
  function getSplitPoint(length) {
-	 return Math.floor(length / 2 + ((Math.random() * 2 * length / 5) - (length / 5)));
+   return Math.floor(length / 2 + ((Math.random() * 2 * length / 5) - (length / 5)));
  }
 
 /**
  * @returns
- * 		The mutation rate of the fish
+ *     The mutation rate of the fish
  */
 function getMutationRate() {
-	return .06;
+  return .06;
 }
--- public/javascripts/models/Neuron.js
+++ public/javascripts/models/Neuron.js
@@ -1,68 +1,68 @@
 /**
  * A class for a basic neuron within a Net.
  * @param numOutputs
- * 		Integer for the total number of edges the neuron connects to
- * 		in the next layer.
+ *     Integer for the total number of edges the neuron connects to
+ *     in the next layer.
  * @param index
  * @param connections
- * 		Array of integers for the weights of the edges
+ *     Array of integers for the weights of the edges
  */
 var Neuron = function(numOutputs, index) {
-	this.eta = 0.39; // Overall net learning rate
-	this.alpha = 0.1; //momentum, multiplied by last delta weight
-	this.numOutputs = numOutputs;
-	this.index = index;
-	this.connectionWeights = buildConnections(numOutputs);
-	
-	this.outputVal = 0.0;
-	this.inputVal = 0.0;
-	this.gradient = 0.0;
+  this.eta = 0.39; // Overall net learning rate
+  this.alpha = 0.1; //momentum, multiplied by last delta weight
+  this.numOutputs = numOutputs;
+  this.index = index;
+  this.connectionWeights = buildConnections(numOutputs);
+
+  this.outputVal = 0.0;
+  this.inputVal = 0.0;
+  this.gradient = 0.0;
 };
 /**
  * @param numOutputs
- * 		The number of outputs 
+ *     The number of outputs
  * @returns
- * 		Array of integers for the weights of each connection.
+ *     Array of integers for the weights of each connection.
  */
 function buildConnections(numOutputs) {
-	var connections = [];
-	for (var index = 0; index < numOutputs; index++) {
-		connections.push((Math.random() * 2) - 1);
-		//connections.push(.1);
-	}
-	return connections;
+  var connections = [];
+  for (var index = 0; index < numOutputs; index++) {
+    connections.push((Math.random() * 2) - 1);
+    //connections.push(.1);
+  }
+  return connections;
 }
 /**
  * Sets the edge weights for each connection on the neuron.
  * @param weights
- * 		an array of flaots for the weights to set
+ *     an array of flaots for the weights to set
  */
  Neuron.prototype.setWeights = function(weights) {
-	 
+
  };
 /**
  * @param x
- * 		The input for the hyperbolic tangent function.
+ *     The input for the hyperbolic tangent function.
  * @returns
- * 		The output of the hyperbolic tangent function.
+ *     The output of the hyperbolic tangent function.
  */
  function transferFunction(x) {
-	 return Math.tanh(x);
+   return Math.tanh(x);
  }
  /**
   * Loops through all previous layers ouputs and sum their outputValue (s)
   * times their weight (s), updating the output value of this neuron.
   * @param prevLayer
-  * 	Array of Neurons from previous layer.
+  *   Array of Neurons from previous layer.
   */
  Neuron.prototype.feedForward = function(prevLayer) {
-	  var sum = 0.0;
-	  for (var index = 0; index < prevLayer.length; index++) {
-		  sum += prevLayer[index].outputVal * 
-		  		prevLayer[index].connectionWeights[this.index];
-	  }
-	  //console.log(sum);
-	  sum/=(prevLayer.length/2.0);
-	  //console.log(sum);
-	  this.outputVal = transferFunction(sum);
+    var sum = 0.0;
+    for (var index = 0; index < prevLayer.length; index++) {
+      sum += prevLayer[index].outputVal *
+          prevLayer[index].connectionWeights[this.index];
+    }
+    //console.log(sum);
+    sum/=(prevLayer.length/2.0);
+    //console.log(sum);
+    this.outputVal = transferFunction(sum);
   }
