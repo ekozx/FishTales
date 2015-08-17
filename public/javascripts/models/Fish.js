77ab363505961ec74beb4b5e826b98393c1e4e8b
Evan Kozliner <evankozliner@gmail.com>
2015-08-17 11:38:47 -0400

    whitespace fix

--- public/javascripts/models/Fish.js
+++ public/javascripts/models/Fish.js
@@ -15,37 +15,37 @@ var Fish = function() {
 Fish.prototype.makeChild = function (maleParent) {
 	var motherChromosome = this.net.getChromosome();
 	var fatherChromosome = maleParent.net.getChromosome();
-	
+
 	if (fatherChromosome.length !== motherChromosome.length) {
 		console.log("Warning: Mother and father chromosomes are not same length!");
 	}
-	
+
 	var splitPoint = getSplitPoint(motherChromosome.length);
 	var motherSide = motherChromosome.splice(0, splitPoint);
-	var fatherSide = fatherChromosome.splice(splitPoint, fatherChromosome.length); 
+	var fatherSide = fatherChromosome.splice(splitPoint, fatherChromosome.length);
 	var joinedChromosome = motherSide.concat(fatherSide);
-	
+
 //	if(joinedChromosome.length !== fatherChromosome.length) {
 //		console.log("Joined chromosome has an incorrect size!");
 //		console.log("FATHER LEN: "+ fatherChromosome.length);
 //		console.log("MOTHER LEN: "+ motherChromosome.length);
 //		console.log("BABY   LEN: "+ joinedChromosome.length);
 //	}
-//	
+//
 	for (var i = 0; i < joinedChromosome.length; i++) {
 		if(Math.random() < getMutationRate()) {
 			joinedChromosome[i] = Math.random() * 2 - 1;
 		}
 	}
-	
+
 	var fish = new Fish();
-	
+
 	fish.circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 20);
-			
+
 	fish.circle.x = Math.random() * w;
 	fish.circle.y = Math.random() * h;
-			
-			
+
+
 	fish.circle.velY = 5;
 	fish.circle.velX = 0;
 
@@ -59,13 +59,13 @@ Fish.prototype.makeChild = function (maleParent) {
 
 	fish.circle.velX = px;
 	fish.circle.velY = py;
-	
+
 	fish.tick=0;
 	fish.fitness=0;
 	fish.dead=0;
 	fish.hunder=100;
-	
-	
+
+
 	fish.net.setChromosome(joinedChromosome);
 	return fish;
 };
@@ -75,9 +75,9 @@ Fish.prototype.makeChild = function (maleParent) {
  * 		An integer, x, s.t. 0<= x <= 10
  */
  function getSplitPoint(length) {
-	 return Math.floor(length / 2 + ((Math.random() * 2 * length / 5) - (length / 5))); 
+	 return Math.floor(length / 2 + ((Math.random() * 2 * length / 5) - (length / 5)));
  }
- 
+
 /**
  * @returns
  * 		The mutation rate of the fish
--- public/javascripts/models/Net.js
+++ public/javascripts/models/Net.js
@@ -7,106 +7,105 @@
  * @param topology
  * 		An array of integers
  */
- var Net = function(topology) {
-	 this.numLayers = topology.length;
-	 this.layers = createLayers(topology);
- };
+var Net = function(topology) {
+	this.numLayers = topology.length;
+  this.layers = createLayers(topology);
+};
 /**
- * Generate each layer for the neural network, populating with regular neurons 
- * and a bias neuron at the end. 
+ * Generate each layer for the neural network, populating with regular neurons
+ * and a bias neuron at the end.
  * @param topology
  * 		An array of integers
  * @returns
  * 		A list of lists of type Neuron
  */
- function createLayers(topology) {
-	 var layers = [];
-	 for (var index = 0; index < topology.length; index++) {
-		 var layer = [];
-		 layers.push(layer);
-		 var numOutputs = (index == topology.length - 1) ? 0 : topology[index  + 1];
-		 for (var neuronNum = 0; neuronNum <= topology[index]; neuronNum++) {
-			 layers[lastIndex(layers)].push(new Neuron(numOutputs, neuronNum));
-		 }
-		 //The bias neuron
-		 layers[lastIndex(layers)][layers[lastIndex(layers)].length - 1].outputVal = 1.0;
-	 }
-
-	 return layers;
- }
- /**
+function createLayers(topology) {
+	var layers = [];
+	for (var index = 0; index < topology.length; index++) {
+		var layer = [];
+		layers.push(layer);
+		var numOutputs = (index == topology.length - 1) ? 0 : topology[index  + 1];
+		for (var neuronNum = 0; neuronNum <= topology[index]; neuronNum++) {
+			layers[lastIndex(layers)].push(new Neuron(numOutputs, neuronNum));
+		}
+		//The bias neuron
+		layers[lastIndex(layers)][layers[lastIndex(layers)].length - 1].outputVal = 1.0;
+	}
+	return layers;
+}
+/**
   * Retruns the result values from the last neurons after calling feedForward.
   * @returns
   * 	An array of floats for the results the network
   */
- Net.prototype.getResults = function() {
-	 var resultVals = [];
-	 for (var index = 0; index < this.layers[this.layers.length -1].length; index++) {
-		 resultVals.push(this.layers[lastIndex(this.layers)][index].outputVal);
-	 }
-	 return resultVals;
- };
- /**
-  * Feeds the inputVals into the initial layer of neurons, 
-  * then uses those initial values to populate outputvals the rest of 
+Net.prototype.getResults = function() {
+	var resultVals = [];
+	for (var index = 0; index < this.layers[this.layers.length -1].length; index++) {
+		resultVals.push(this.layers[lastIndex(this.layers)][index].outputVal);
+	}
+  return resultVals;
+};
+/**
+  * Feeds the inputVals into the initial layer of neurons,
+  * then uses those initial values to populate outputvals the rest of
   * the neurons in the network.
   * @param inputVals
   * 	An array of floats, x, s.t. 0 <= x < 1
   */
-  Net.prototype.feedForward = function(inputVals) {
-	  if(inputVals.length !== this.layers[0].length-1) {
-		  console.log("Incorrect number of inputs for the first network layer!");
-		  console.log("Expect incorrect output!");
+Net.prototype.feedForward = function(inputVals) {
+  if(inputVals.length !== this.layers[0].length-1) {
+	  console.log("Incorrect number of inputs for the first network layer!");
+	  console.log("Expect incorrect output!");
+  }
+  for (var i = 0; i < inputVals.length; i++) { // populate the first layer
+	  this.layers[0][i].outputVal = inputVals[i];
+  }
+  // loop through each layer but the first, using the previous layer to get
+  // output values for each of the present layers neurons with feedFoward.
+  for (var layerNum = 1; layerNum < this.layers.length; layerNum++) {
+	  var prevLayer = this.layers[layerNum - 1];
+	  for (var j = 0; j < this.layers[layerNum].length - 1; j++) {
+		  this.layers[layerNum][j].feedForward(prevLayer);
 	  }
-	  for (var i = 0; i < inputVals.length; i++) { // populate the first layer
-		  this.layers[0][i].outputVal = inputVals[i];
-	  }
-	  // loop through each layer but the first, using the previous layer to get
-	  // output values for each of the present layers neurons with feedFoward.
-	  for (var layerNum = 1; layerNum < this.layers.length; layerNum++) {
-		  var prevLayer = this.layers[layerNum - 1];
-		  for (var j = 0; j < this.layers[layerNum].length - 1; j++) {
-			  this.layers[layerNum][j].feedForward(prevLayer);
-		  }
-	  }
-  };
-  /**
-   *  Returns the chromosome from a single net. The chromosome is a list of 
-   *  all of the outputs of each neuron in the network (edges in the graph).
-   *  @returns
-   * 	an array of floats
-   */
-   Net.prototype.getChromosome = function() {
-	   var chromosome = [];
-	   this.layers.forEach(function (neurons, i) {
-		   neurons.forEach(function (neuron, j) {
-			  neuron.connectionWeights.forEach(function (connection, k) {
-				 chromosome.push(connection);  
-			  }); 
-		   });
-	   });  
-	   return chromosome;
-   };
-   /**
+  }
+};
+/**
+ *  Returns the chromosome from a single net. The chromosome is a list of
+ *  all of the outputs of each neuron in the network (edges in the graph).
+ *  @returns
+ * 	an array of floats
+ */
+Net.prototype.getChromosome = function() {
+  var chromosome = [];
+  this.layers.forEach(function (neurons, i) {
+	  neurons.forEach(function (neuron, j) {
+		  neuron.connectionWeights.forEach(function (connection, k) {
+			chromosome.push(connection);
+		  });
+	  });
+  });
+  return chromosome;
+};
+/**
 	* Sets the list of all neuron connection weights from the graph.
 	* @param weights
 	* 	An array of floats, containing weights in order from first in the top
 	* 	layer to last.
-    */
-	Net.prototype.setChromosome = function(weights) {
-		weights.reverse(); // reverse the array because pop takes the last element
-		this.layers.forEach(function(neurons, i) {
-			neurons.forEach(function(neuron, j) {
-				neuron.connectionWeights = []; // reset the neurons edges
-				for (var k = 0; k < neuron.numOutputs; k++) {
-					neuron.connectionWeights.push(weights.pop()); //add in the new edge
-				}
-			});
+  */
+Net.prototype.setChromosome = function(weights) {
+	weights.reverse(); // reverse the array because pop takes the last element
+	this.layers.forEach(function(neurons, i) {
+		neurons.forEach(function(neuron, j) {
+			neuron.connectionWeights = []; // reset the neurons edges
+			for (var k = 0; k < neuron.numOutputs; k++) {
+			  neuron.connectionWeights.push(weights.pop()); //add in the new edge
+			}
 		});
-	};
-	/**
-	 * a small helper function for less lengthy code
-	 */
-	 function lastIndex(layers) {
-		 return layers.length - 1;
-	 }
+	});
+};
+/**
+ * a small helper function for less lengthy code
+ */
+function lastIndex(layers) {
+	return layers.length - 1;
+}
